85b1bd0342f856812e1a710edea34f6514582932
Python111001 <user.new22@mail.ru>
2023-11-21 21:57:18 +0600

    pizza, categories, size, type

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(2)
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map((value, index)=>(
+                    <li onClick={()=>setActiveIndex(index)} className={activeIndex === index ? 'active': ''}>{value}</li>
+                ))}
             </ul>
         </div>
     );
